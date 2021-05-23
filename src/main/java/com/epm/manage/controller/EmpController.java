f9f72f2af475261ae1e09de106c42ddc00c2ff79
Megha Parmar <4931343-megha_8221@users.noreply.gitlab.com>
2021-05-23 12:24:56 +0530

    Emp crud finish

--- src/main/java/com/epm/manage/controller/EmpController.java
+++ src/main/java/com/epm/manage/controller/EmpController.java
@@ -1,4 +1,48 @@
 package com.epm.manage.controller;
 
+import com.epm.manage.model.Emp;
+import com.epm.manage.model.Project;
+import com.epm.manage.repositories.EmpRepositories;
+import com.epm.manage.repositories.ProjectRepositories;
+import com.epm.manage.service.EmpService;
+import com.epm.manage.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping(path = "api/v1/emp")
 public class EmpController {
+    @Autowired
+    private EmpService empService;
+    @Autowired
+    private EmpRepositories empRepositories;
+
+    @PostMapping
+    public ResponseEntity addEmp(@RequestBody Emp emp){
+        Emp res = empService.addEmpService(emp);
+        return ResponseEntity.ok(res);
+    }
+    @PutMapping(path = "/{empId}")
+    public ResponseEntity updateEmp(@PathVariable("empId") long empId, @RequestBody Emp emp){
+        Emp res = empService.updateEmpService(empId,emp);
+        return ResponseEntity.ok(res);
+    }
+    @DeleteMapping(path = "/{empId}")
+    public String removeEmp(@PathVariable("empId") long empId){
+
+        return empService.removeEmp(empId);
+
+    }
+    @GetMapping
+    public ResponseEntity getEmps(){
+        List res = empService.getEmp();
+        return ResponseEntity.ok(res);
+    }
+    @GetMapping(path = "/{empId}")
+    public ResponseEntity getEmpById(@PathVariable("empId") long empId){
+        List res = empService.getEmpById(empId);
+        return ResponseEntity.ok(res);
+    }
 }
--- src/main/java/com/epm/manage/service/EmpService.java
+++ src/main/java/com/epm/manage/service/EmpService.java
@@ -1,10 +1,54 @@
 package com.epm.manage.service;
 
+import com.epm.manage.model.Emp;
 import com.epm.manage.model.Project;
+import com.epm.manage.repositories.EmpRepositories;
+import com.epm.manage.repositories.ProjectRepositories;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class EmpService {
+    @Autowired
+    private EmpRepositories empRepositories;
 
+    public Emp addEmpService(Emp emp){
+        return empRepositories.save(emp);
+    }
+
+    public List getEmp() {
+        return  empRepositories.findAll();
+    }
+
+    public List getEmpById( long empId) {
+        boolean exists = empRepositories.existsById(empId);
+        if(!exists){
+            throw new IllegalStateException("not found emp");
+        }
+        return empRepositories.findAllById(Collections.singleton(empId));
+    }
+
+    @Transactional
+    public Emp updateEmpService(long empId, Emp emp) {
+        boolean exists = empRepositories.existsById(empId);
+        if(!exists){
+            throw new IllegalStateException("not found emp");
+        }
+        emp.setId(empId);
+        return empRepositories.save(emp);
+    }
+
+    public String removeEmp(long empId) {
+        boolean exists = empRepositories.existsById(empId);
+        if(!exists){
+            throw new IllegalStateException("not found emp");
+        }
+        empRepositories.deleteAllById(Collections.singleton(empId));
+        return "record Deleted";
+    }
 
 }
